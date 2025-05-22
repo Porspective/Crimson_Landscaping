887f9c3f7fce90688d36805bb6bc276d11f9b2fd
Porspective <porter.j.robertson@gmail.com>
2025-05-22 15:41:36 -0500

    Restore files to previous state

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Crimson_Landscaping/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
