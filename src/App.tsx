f68e297a2bbd22842d912b22181d30fe1162d069
Porspective <porter.j.robertson@gmail.com>
2025-05-22 19:34:18 -0500

    Remove build-specific asset references

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
@@ -17,6 +17,9 @@ function App() {
 
   return (
     <Routes>
+      {/* Redirect GitHub Pages path to home */}
+      <Route path="/Crimson_Landscaping/*" element={<Navigate to="/" replace />} />
+
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="services" element={<ServicesPage />} />
