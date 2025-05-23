53b45abd1e57cff0251a93d0f7cd197efc53b191
Porspective <porter.j.robertson@gmail.com>
2025-05-22 19:32:48 -0500

    Remove build-specific asset references

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Crimson_Landscaping">
       <App />
     </BrowserRouter>
   </StrictMode>
