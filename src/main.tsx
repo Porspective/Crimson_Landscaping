a0e271a3bc789c98e810e53c864e75469cfa478c
Porspective <porter.j.robertson@gmail.com>
2025-05-22 20:01:04 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/Crimson_Landscaping">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
