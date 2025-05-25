e38fb21f60ecec69b34179231d220faea2188f45
Porspective <porter.j.robertson@gmail.com>
2025-05-25 10:10:41 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
