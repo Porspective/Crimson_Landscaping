c803b96a883d7ad849239b43b6621188094b6a7e
Porspective <porter.j.robertson@gmail.com>
2025-05-30 15:09:17 -0500

    Fix preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   server: {
-    headers: { "Content-Type": "text/javascript" }
+    port: 3000,
+    open: true
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js'
-      }
-    }
+  preview: {
+    port: 3000
   }
 });
