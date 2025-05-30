4467c397b33e4355e74c9498e38a68a74bc1070e
Porspective <porter.j.robertson@gmail.com>
2025-05-30 15:50:12 -0500

    Revert Netlify Functions setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
-    open: true
+    headers: { "Content-Type": "text/javascript" }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js'
+      }
+    }
   }
 });
