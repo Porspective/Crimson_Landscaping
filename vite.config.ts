6c0ed8dd519fa50bf7f124b03ba5bacda58aaf3b
Porspective <porter.j.robertson@gmail.com>
2025-05-31 13:53:24 -0500

    Fix image paths and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets',
+    copyPublicDir: true,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
   }
