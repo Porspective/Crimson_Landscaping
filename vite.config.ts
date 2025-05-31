4752bad478631b217c550c6a475432cb4b04c8be
Porspective <porter.j.robertson@gmail.com>
2025-05-31 02:55:57 -0500

    Restore working configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crimson-landscaping/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
