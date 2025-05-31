ee7438362991a268964d3538c42cc456aad83458
Porspective <porter.j.robertson@gmail.com>
2025-05-31 01:03:55 -0500

    Create new Vite project with React and TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
-import ServicesPage from './pages/ServicesPage';
-import ContactPage from './pages/ContactPage';
-import NotFoundPage from './pages/NotFoundPage';
-import PaymentSuccessPage from './pages/PaymentSuccessPage';
 
-function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
-
+const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="services" element={<ServicesPage />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="payment-success" element={<PaymentSuccessPage />} />
-        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
